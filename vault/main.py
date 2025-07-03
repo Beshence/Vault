0ec310d31cfa3176c1485ac0ad9e05eb92d380d4
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-03 16:17:36 +0300

    FastAPI Users

--- vault/main.py
+++ vault/main.py
@@ -1,25 +1,37 @@
-import uvicorn
-from fastapi import FastAPI, APIRouter
-from fastapi_versionizer import api_version
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 
 from fastapi_versionizer.versionizer import Versionizer
 
 from vault.api import api_router
+from vault.core.db import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Not needed if you set up a migration system like Alembic
+    await create_db_and_tables()
+    yield
 
 app = FastAPI(
-    root_path='/api',
+    root_path='',
     title='Beshence Vault',
-    docs_url='/docs',
     redoc_url=None,
-    description='Beshence Vault'
+    description='Beshence Vault',
+    lifespan=lifespan
 )
 
 app.include_router(api_router)
 
-versions = Versionizer(
+api_versions = Versionizer(
     app=app,
-    prefix_format='/v{major}.{minor}',
+    default_version=(1, 0),
+    prefix_format='/api/v{major}.{minor}',
     semantic_version_format='{major}.{minor}',
-    latest_prefix='/latest',
-    sort_routes=True
+    latest_prefix='/api/latest',
+    sort_routes=True,
+    include_main_docs=False,
+    include_main_openapi_route=False
 ).versionize()
+
+app.state.latest_api_version = "v"+".".join(map(str, api_versions[-1]))
