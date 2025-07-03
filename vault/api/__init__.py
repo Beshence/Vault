198bb1fec23c2d20cf1209cefe55151e99bd027b
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-03 22:52:12 +0300

    теперь юзеры по username хранятся а не по email

--- vault/api/__init__.py
+++ vault/api/__init__.py
@@ -20,35 +20,24 @@ api_router.include_router(
     prefix="/auth",
     tags=["Auth"],
 )
-
-api_router.include_router(
+"""api_router.include_router(
     fastapi_users.get_reset_password_router(),
     prefix="/auth",
     tags=["Auth"],
-)
-api_router.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["Auth"],
-)
+)"""
 api_router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/user",
     tags=["Users"],
 )
 
-@api_version(1, 0)
+"""@api_version(1, 0)
 @api_router.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
-    return {"message": f"Hello {user.email}!"}
+    return {"message": f"Hello {user.email}!"}"""
 
 
 @api_version(1, 0)
-@api_router.get('/ping', tags=['Common'])
-def ping(request: Request) -> dict:
-    return {"ping": "pong", "latest": request.app.state.latest_api_version}
-
-@api_version(1, 1)
 @api_router.get('/ping', tags=['Common'])
 def ping(request: Request) -> dict:
     return {"ping": "pong", "latest": request.app.state.latest_api_version}
