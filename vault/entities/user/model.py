970811f7d40cab980b77ce51401e15ba41481211
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-04 00:08:01 +0300

    теперь вся эта мешанина с email и username в отдельном репозитории

--- vault/entities/user/model.py
+++ vault/entities/user/model.py
@@ -1,28 +1,12 @@
-import uuid
-from datetime import datetime
-
-from fastapi_users_db_sqlalchemy import UUID_ID, GUID
-from fastapi_users_db_sqlalchemy.generics import TIMESTAMPAware, now_utc
-from sqlalchemy import Column, String, Boolean, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
 
 from vault.core.db import Base
 
 
-class User(Base):
-    __tablename__ = "Users"
-
-    id: Mapped[UUID_ID] = mapped_column(GUID, primary_key=True, default=uuid.uuid4)
-    username = Column(String(length=320), unique=True, index=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
-    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    pass
 
 
-
-class AccessToken(Base):
-    __tablename__ = "AccessTokens"
-
-    user_id: Mapped[GUID] = mapped_column(GUID, ForeignKey("Users.id", ondelete="cascade"), nullable=False)
-    token: Mapped[str] = mapped_column(String(length=43), primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMPAware(timezone=True), index=True, nullable=False, default=now_utc)
+class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):
+    pass
--- vault/entities/user/schema.py
+++ vault/entities/user/schema.py
@@ -1,48 +1,15 @@
 import uuid
-from typing import Optional, Generic
 
-from fastapi_users import models
-from fastapi_users.schemas import model_dump, PYDANTIC_V2
-from pydantic import BaseModel, ConfigDict
+from fastapi_users import schemas
 
 
-class UserModel(BaseModel):
-    def create_update_dict(self):
-        return model_dump(
-            self,
-            exclude_unset=True,
-            exclude={
-                "id",
-                "is_superuser",
-                "is_active"
-            },
-        )
-
-    def create_update_dict_superuser(self):
-        return model_dump(self, exclude_unset=True, exclude={"id"})
-
-class UserRead(UserModel, Generic[models.ID]):
-    id: uuid.UUID
-    username: str
-    is_active: bool = True
-    is_superuser: bool = False
-
-    if PYDANTIC_V2:  # pragma: no cover
-        model_config = ConfigDict(from_attributes=True)  # type: ignore
-    else:  # pragma: no cover
-        class Config:
-            orm_mode = True
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
 
 
-class UserCreate(UserModel):
-    username: str
-    password: str
-    #is_active: Optional[bool] = True
-    #is_superuser: Optional[bool] = False
+class UserCreate(schemas.BaseUserCreate):
+    pass
 
 
-class UserUpdate(UserModel):
-    username: Optional[str] = None
-    password: Optional[str] = None
-    #is_active: Optional[bool] = None
-    #is_superuser: Optional[bool] = None
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
