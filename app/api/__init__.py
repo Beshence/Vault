302119685c84129aafbf35611ac6d724eb2f1597
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-19 18:11:32 +0300

    remove fastapi-users entirely as this library is not flexible at all

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,46 +1,21 @@
-from typing import Optional
-
-from fastapi import APIRouter, Request, Depends
-from fastapi_users import schemas
+from fastapi import APIRouter, Request
 from fastapi_versionizer import api_version
 from pydantic import BaseModel
 
 from app.api.user import user_router
-from app.entities.user.manager import fastapi_users, auth_backend, current_user
-from app.entities.user.model import User
-from app.entities.user.schema import UserRead, UserCreate
 
 api_router = APIRouter()
-
-api_router.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth",
-    tags=["Auth"]
-)
-
-api_router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["Auth"],
-)
-"""api_router.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["Auth"],
-)"""
 api_router.include_router(user_router)
 
 
 class Ping(BaseModel):
     ping: str = "pong"
     latest_api_version: str = "v0.0"
-    me: UserRead | None
 
 @api_version(1, 0)
 @api_router.get('/ping', tags=['Common'])
-def ping(request: Request, me: Optional[User] = Depends(current_user(optional=True))) -> Ping:
+def ping(request: Request) -> Ping:
     return Ping(
         ping="pong",
-        latest_api_version=request.app.state.latest_api_version,
-        me=schemas.model_validate(UserRead, me) if me else None
+        latest_api_version=request.app.state.latest_api_version
     )
--- app/api/user/__init__.py
+++ app/api/user/__init__.py
@@ -1,9 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.user.repo import repo_router
-from app.entities.user.manager import fastapi_users
-from app.entities.user.schema import UserRead, UserUpdate
 
 user_router = APIRouter(prefix="/user")
-user_router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), tags=["Users"])
 user_router.include_router(repo_router)
