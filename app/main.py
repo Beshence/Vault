15e99bee1c9bde6b37e9448c1ba172c33c2a96c5
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-20 15:53:47 +0300

    added cors headers

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from fastapi_versionizer.versionizer import Versionizer
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import api_router
 
@@ -17,6 +18,14 @@ app = FastAPI(
     redoc_url=None,
     description='Beshence Vault',
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
