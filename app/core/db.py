96a571d88daef238049ea505969ec764f14d2f05
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-08-23 14:34:23 +0300

    created session dependency

--- app/core/db.py
+++ app/core/db.py
@@ -1,5 +1,6 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Annotated
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
@@ -17,3 +18,5 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_async_session)]
