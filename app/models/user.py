93474a1f5e19b550661c609a40378eb426806bd8
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-08-23 15:55:44 +0300

    changes... once again

--- app/models/user.py
+++ app/models/user.py
@@ -4,11 +4,11 @@ from sqlmodel import SQLModel, Field, String, UUID
 class UserBase(SQLModel):
     id: UUID = Field(primary_key=True, nullable=False, index=True)
     username: str = Field(nullable=False, unique=True, index=True)
+    is_active: bool = Field(nullable=False)
 
 class User(UserBase, table=True):
-    password: str = Field(nullable=True)
+    password: str | None = Field(nullable=True)
     fast_login_secret: String = Field(nullable=False)
-    is_active: bool = Field(nullable=False)
 
 class UserPublic(UserBase):
     pass
