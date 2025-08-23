7e6462e6e1ce5942f0808e6655bb94eac54eec96
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-08-23 15:40:08 +0300

    and models should be aware of past changes too

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,10 @@ from sqlmodel import SQLModel, Field, String, UUID
 
 class UserBase(SQLModel):
     id: UUID = Field(primary_key=True, nullable=False, index=True)
+    username: str = Field(nullable=False, unique=True, index=True)
 
 class User(UserBase, table=True):
+    password: str = Field(nullable=True)
     fast_login_secret: String = Field(nullable=False)
     is_active: bool = Field(nullable=False)
 
